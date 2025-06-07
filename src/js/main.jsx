0fbbcb63f8aa116a8cda9d5f2efabc669a28d39c
Jorge Sanchez Ortiz <jorgesoibz@gmail.com>
2025-06-07 08:40:53 +0000

    semaforo

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,10 +9,11 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+
+import TrafficLight from './components/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <TrafficLight/>
   </React.StrictMode>,
 )
